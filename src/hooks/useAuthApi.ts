64f74fc86c8d0670c8bba9fed14741bf2ecd207e
Yeong joon <bluehome8626@naver.com>
2023-08-16 09:44:06 +0900

    refactoring : mod type name

--- src/hooks/useAuthApi.ts
+++ src/hooks/useAuthApi.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import type { UserAccount, UserInfo } from "../types/User";
+import type { UserAccount, UserInfo } from "../types/user";
 import { useMutation, useQuery } from "react-query";
 import { useNavigate } from "react-router-dom";
 import { useUserInfoStore } from "../store";
